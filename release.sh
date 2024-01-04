5dc35529477c8799641d2173899b358535ca985c
Jordan Acosta <jordan@nuon.co>
2024-01-03 17:50:39 -0800

    test: add basic integration test to pr checks (#4)
    * test: add basic integration test to pr checks
    
    * fix: fail action if missing build_id and component_id

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -n $BUILD_ID ]
-then
-  nuon releases create -j -b $BUILD_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
-elif [ -n $COMPONENT_ID ]
-then
-  nuon releases create -j -b $COMPONENT_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
+if [ -z "$BUILD_ID" ]; then
+    echo "releasing build \"$BUILD_ID\""
+    nuon releases create -j -b $BUILD_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
+elif [ -z "$COMPONENT_ID" ]; then
+    echo "releasing latest build of component \"$COMPONENT_ID\""
+    nuon releases create -j -b $COMPONENT_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
 else
-  echo "Require either a build ID or component ID to create a release."
-  exit 1
+    echo "Require either a build ID or component ID to create a release."
+    exit 1
 fi
