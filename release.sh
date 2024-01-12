b921b026e32a95ac803a2ed44737ba788cc78b4b
Jordan Acosta <jordanjacosta@gmail.com>
2024-01-11 22:10:35 -0800

    feat: expose auto-build option

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ if [ -z "$BUILD_ID" ]; then
     nuon releases create -j -b $BUILD_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
 elif [ -z "$COMPONENT_ID" ]; then
     echo "releasing latest build of component \"$COMPONENT_ID\""
-    nuon releases create -j -b $COMPONENT_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
+    nuon releases create -j -b $COMPONENT_ID --delay $DELAY --installs-per-step $INSTALLS_PER_STEP --auto-build $AUTO_BUILD | jq -r '"RELEASE_ID=\(.id)"' >> $GITHUB_OUTPUT
 else
     echo "Require either a build ID or component ID to create a release."
     exit 1
